f027c71ec51b22146ba7b9e2e298b0e81d4d520a
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-08-02 23:36:33 +0300

    add global conftest

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+from os import getenv
+
+import pytest
+
+
+@pytest.fixture(scope="session", autouse=True)
+def check_testing_env() -> None:
+    if getenv("TESTING") != "1":
+        pytest.exit(
+            "Environment is not ready for testing",
+        )
