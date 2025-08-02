12eda52fe5ffc812eed12d22c25ec4038cc4d777
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-08-02 14:52:55 +0300

    add project config

---
+++ url_shortener/config.py
@@ -0,0 +1,108 @@
+import logging
+from pathlib import Path
+from typing import Literal, Self
+
+from pydantic import BaseModel, model_validator
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    YamlConfigSettingsSource,
+)
+
+BASE_DIR = Path(__file__).resolve().parent
+
+
+class LoggingConfig(BaseModel):
+    log_format: str = (
+        "[-] %(asctime)s [%(levelname)s] %(module)s-%(lineno)d - %(message)s"
+    )
+    log_level_name: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "WARNING"
+    log_date_format: str = "%Y-%m-%d %H:%M:%S"
+
+    @property
+    def log_level(self) -> int:
+        return logging.getLevelNamesMapping()[self.log_level_name]
+
+
+class RedisConnectionConfig(BaseModel):
+    host: str = "localhost"
+    port: int = 6379
+
+
+class RedisDatabaseConfig(BaseModel):
+    default: int = 0
+    tokens: int = 1
+    users: int = 2
+    url_shortener: int = 3
+
+    @model_validator(mode="after")
+    def validate_dbs_numbers_unique(self) -> Self:
+        db_value = list(self.model_dump().values())
+        if len(set(db_value)) != len(db_value):
+            raise ValueError("Database numbers must have unique values")
+        return self
+
+
+class RedisCollectionsNamesConfig(BaseModel):
+    tokens_set: str = "tokens"
+    url_shortener_hash: str = "url-shortener"
+
+
+class RedisConfig(BaseModel):
+    connection: RedisConnectionConfig = RedisConnectionConfig()
+    db: RedisDatabaseConfig = RedisDatabaseConfig()
+    collections: RedisCollectionsNamesConfig = RedisCollectionsNamesConfig()
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        case_sensitive=False,
+        env_file=(
+            BASE_DIR / ".env.template",
+            BASE_DIR / ".env",
+        ),
+        env_prefix="SHORTENER__",
+        env_nested_delimiter="__",
+        yaml_file=(
+            BASE_DIR / "config.default.yaml",
+            BASE_DIR / "config.local.yaml",
+        ),
+        yaml_config_section="shortener",
+    )
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        """
+        Define the sources and their order for loading the settings values.
+
+        Args:
+            settings_cls: The Settings class.
+            init_settings: The `InitSettingsSource` instance.
+            env_settings: The `EnvSettingsSource` instance.
+            dotenv_settings: The `DotEnvSettingsSource` instance.
+            file_secret_settings: The `SecretsSettingsSource` instance.
+
+        Returns:
+            A tuple containing the sources and their order for loading the settings values.
+        """
+        return (
+            init_settings,
+            env_settings,
+            dotenv_settings,
+            file_secret_settings,
+            YamlConfigSettingsSource(settings_cls),
+        )
+
+    logging: LoggingConfig = LoggingConfig()
+    redis: RedisConfig = RedisConfig()
+
+
+settings = Settings()
