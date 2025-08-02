3eb3d13f7a10a1e1245eeb2f814f9dd69766f499
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-08-02 14:58:16 +0300

    add api endpoints for short url

---
+++ url_shortener/api/short_urls/views/details_views.py
@@ -0,0 +1,53 @@
+from typing import Annotated
+
+from api.short_urls.crud import storage
+from fastapi import APIRouter, Depends, status
+from schemas.short_url import ShortUrlPartialUpdate, ShortUrlUpdate
+
+from url_shortener.api.short_urls.dependencies import prefetch_short_url
+from url_shortener.schemas.short_url import ShortUrl, ShortUrlRead
+
+router = APIRouter(prefix="/{slug}")
+
+ShortUrlBySlug = Annotated[
+    ShortUrl,
+    Depends(prefetch_short_url),
+]
+
+
+@router.get(
+    "/",
+    response_model=ShortUrlRead,
+)
+def read_short_url_details(
+    short_url: ShortUrlBySlug,
+) -> ShortUrl:
+    return short_url
+
+
+@router.put(
+    "/",
+    response_model=ShortUrlRead,
+)
+def update_short_url_details(
+    url: ShortUrlBySlug, short_url_in: ShortUrlUpdate
+) -> ShortUrl:
+    return storage.update(short_url=url, short_url_in=short_url_in)
+
+
+@router.patch(
+    "/",
+    response_model=ShortUrlRead,
+)
+def partial_update_short_url_details(
+    url: ShortUrlBySlug, short_url_in: ShortUrlPartialUpdate
+) -> ShortUrl:
+    return storage.update_partial(short_url=url, short_url_in=short_url_in)
+
+
+@router.delete(
+    "/",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+def delete_short_url(url: ShortUrlBySlug) -> None:
+    storage.delete(short_url=url)
---
+++ url_shortener/api/short_urls/views/list_views.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, HTTPException, status
+
+from url_shortener.api.short_urls.crud import storage
+from url_shortener.exceptions import ShortUrlAlreadyExistsError
+from url_shortener.schemas.short_url import ShortUrl, ShortUrlCreate, ShortUrlRead
+
+router = APIRouter(
+    prefix="/shortener",
+    tags=["Short URLs"],
+)
+
+
+@router.get(
+    "/",
+    response_model=list[ShortUrlRead],
+)
+def get_short_url_list() -> list[ShortUrl]:
+    return storage.get()
+
+
+@router.post(
+    "/",
+    response_model=ShortUrlRead,
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        status.HTTP_409_CONFLICT: {
+            "description": "A short url with such slug already exists.",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "Short URL with slug='name' already exists.",
+                    },
+                },
+            },
+        },
+    },
+)
+def create_short_url(
+    short_url_create: ShortUrlCreate,
+) -> ShortUrl:
+    try:
+        return storage.create_or_rise_if_exists(short_url_create)
+    except ShortUrlAlreadyExistsError:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Short URL with slug={short_url_create.slug!r} already exists.",
+        )
