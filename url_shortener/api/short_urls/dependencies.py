2151e7ea79ac84a43d24e7b25d0c9287e3d1fc75
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-08-02 14:56:27 +0300

    create dependencies for api

---
+++ url_shortener/api/short_urls/dependencies.py
@@ -0,0 +1,15 @@
+from fastapi import HTTPException, status
+
+from url_shortener.api.short_urls.crud import storage
+from url_shortener.schemas.short_url import ShortUrl
+
+
+def prefetch_short_url(slug: str) -> ShortUrl:
+    url: ShortUrl | None = storage.get_by_slug(slug=slug)
+    if url:
+        return url
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"URL {slug!r} not found",
+    )
