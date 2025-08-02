ddebfc6ae7aa746cc0ea22211f4e6a1b21fb1f9d
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-08-02 14:58:52 +0300

    include routers to app with lifespan

--- url_shortener/main.py
+++ url_shortener/main.py
@@ -1,8 +1,22 @@
+import logging
+
+from app_lifespan import lifespan
 from fastapi import FastAPI
 
-app = FastAPI()
+from url_shortener.api import router as api_router
+from url_shortener.api.main_views import router as main_router
+from url_shortener.api.redirect_views import router as redirect_router
+from url_shortener.config import settings
 
+logging.basicConfig(
+    level=settings.logging.log_level,
+    format=settings.logging.log_format,
+    datefmt=settings.logging.log_date_format,
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app = FastAPI(
+    lifespan=lifespan,
+)
+app.include_router(main_router)
+app.include_router(redirect_router)
+app.include_router(api_router)
