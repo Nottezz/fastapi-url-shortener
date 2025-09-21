0756021d3cbeb7220390db54a38da3481ff932b7
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-09-21 17:57:01 +0300

    add render HTML when open root path

--- url_shortener/api/main_views.py
+++ url_shortener/api/main_views.py
@@ -1,17 +1,39 @@
-from datetime import datetime
+from datetime import date, datetime
+from typing import Any
 
 from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
 
+from templating import templates
 from url_shortener.schemas.health import Health
 
 router = APIRouter()
 app_launch_time = datetime.now()
 
 
-@router.get("/")
-def root(
-    request: Request,
-) -> Health:
+@router.get("/", include_in_schema=False)
+def root(request: Request) -> HTMLResponse:
+    context: dict[str, Any] = {}
+    today = date.today()
+    features = [
+        "Managing short URL: create, update, delete",
+        "Custom alias for short links",
+        "Analytics: track clicks, referrers, devices, and geolocation",
+        "Expiration rules: set expiry date or maximum number of clicks",
+        "Password-protected short links",
+        "Bulk creation and import/export of short URLs",
+    ]
+    context.update(
+        today=today,
+        features=features,
+    )
+    return templates.TemplateResponse(
+        request=request, name="home.html", context=context
+    )
+
+
+@router.get("/health")
+def check_health() -> Health:
     uptime = (datetime.now() - app_launch_time).total_seconds()
     docs_url = request.url.replace(
         path="/docs",
