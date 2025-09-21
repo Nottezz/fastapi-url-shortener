429a056c4679d41f97435080541c168e3b398787
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-09-21 18:31:33 +0300

    add today as global context value

--- url_shortener/templating/jinja_templates.py
+++ url_shortener/templating/jinja_templates.py
@@ -1,5 +1,20 @@
+from datetime import date
+
+from fastapi import Request
 from fastapi.templating import Jinja2Templates
 
 from config import BASE_DIR
 
-templates = Jinja2Templates(directory=BASE_DIR / "templates")
+
+def inject_current_date(
+    request: Request,  # noqa: ARG001 Unused function argument: `request`
+) -> dict[str, date]:
+    return {"today": date.today()}
+
+
+templates = Jinja2Templates(
+    directory=BASE_DIR / "templates",
+    context_processors=[
+        inject_current_date,
+    ],
+)
