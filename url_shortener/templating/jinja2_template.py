9e4895819012f69ad6f07ee416e9d2e825b22f4a
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-11-14 16:54:54 +0300

    move to global dependencies prefetch_short_url

--- url_shortener/templating/jinja2_template.py
+++ url_shortener/templating/jinja2_template.py
@@ -2,15 +2,19 @@ from datetime import date
 
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
+
 from url_shortener.config import BASE_DIR
 
+
 def inject_current_date(
-        request: Request,
+    request: Request,  # noqa: ARG001
 ) -> dict[str, date]:
     return {"today": date.today()}
 
-templates = Jinja2Templates(
-    directory= BASE_DIR / "templates",
-    context_processors=[inject_current_date,]
 
+templates = Jinja2Templates(
+    directory=BASE_DIR / "templates",
+    context_processors=[
+        inject_current_date,
+    ],
 )
