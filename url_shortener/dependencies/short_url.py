93c2e2722d9fb2b0b03a83d97116f970531df219
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-11-14 16:35:06 +0300

    move to global dependencies prefetch_short_url

--- url_shortener/dependencies/short_url.py
+++ url_shortener/dependencies/short_url.py
@@ -1,9 +1,31 @@
-from fastapi import Request
+from typing import Annotated
 
+from fastapi import Depends, HTTPException, Request
+from starlette import status
+
+from schemas.short_url import ShortUrl
 from storage.short_url import ShortUrlStorage
+from storage.short_url.crud import storage
 
 
 def get_short_url_storage(
     request: Request,
 ) -> ShortUrlStorage:
     return request.app.state.short_url_storage
+
+
+GetShortUrlStorage = Annotated[ShortUrlStorage, Depends(get_short_url_storage)]
+
+
+def prefetch_short_url(slug: str) -> ShortUrl:
+    url: ShortUrl | None = storage.get_by_slug(slug=slug)
+    if url:
+        return url
+
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail=f"URL <{slug}> not found.",
+    )
+
+
+UrlBySlug = Annotated[ShortUrl, Depends(prefetch_short_url)]
