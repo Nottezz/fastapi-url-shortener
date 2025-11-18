dd7e3c0094364b3322e933ad159c28b9277ec11a
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-11-18 15:24:13 +0300

    add page for update url

--- url_shortener/rest/short_urls/__init__.py
+++ url_shortener/rest/short_urls/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from .create_view import router as create_view
 from .list_view import router as list_view
+from .update_view import router as update_view
 
 router = APIRouter(
     prefix="/shortener",
@@ -9,3 +10,4 @@ router = APIRouter(
 )
 router.include_router(list_view)
 router.include_router(create_view)
+router.include_router(update_view)
