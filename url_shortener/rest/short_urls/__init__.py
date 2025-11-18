b606cf998d52bf6d1550f7c77d63e55296bc39dd
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-11-18 16:08:44 +0300

    add delete action

--- url_shortener/rest/short_urls/__init__.py
+++ url_shortener/rest/short_urls/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
 from .create_view import router as create_view
+from .delete_view import router as delete_view
 from .list_view import router as list_view
 from .update_view import router as update_view
 
@@ -11,3 +12,4 @@ router = APIRouter(
 router.include_router(list_view)
 router.include_router(create_view)
 router.include_router(update_view)
+router.include_router(delete_view)
