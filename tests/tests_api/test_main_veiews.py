08bb0f7d94f417ac2fe39370b34c4cd55d88bd27
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-11-18 16:36:50 +0300

    change root test to template check

--- tests/tests_api/test_main_veiews.py
+++ tests/tests_api/test_main_veiews.py
@@ -2,13 +2,11 @@ import pytest
 from fastapi import status
 from fastapi.testclient import TestClient
 
-pytestmark = pytest.mark.apitest
+pytestmark = pytest.mark.templatetest
 
 
 def test_root(client: TestClient) -> None:
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK, response.text
-    response_json = response.json()
-    assert response_json["status"] == "OK", response_json
-    assert response_json["current_uptime"] == 0, response_json
-    assert response_json["docs_url"] == "http://testserver/docs", response_json
+    assert response.template.name == "home.html"  # type: ignore[attr-defined]
+    assert "today" in response.context, response.context  # type: ignore[attr-defined]
